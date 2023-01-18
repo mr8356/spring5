80d039bdb4f82bd9d28f8316c4ab88f96c1f5eea
DongHyun Jo <mr8356@konkuk.ac.kr>
2023-01-18 14:54:48 +0000

    Member Controll Project

--- boot/src/main/java/example/boot/controller/MemberController.java
+++ boot/src/main/java/example/boot/controller/MemberController.java
@@ -1,8 +1,14 @@
 package example.boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import example.boot.domain.Member;
 import example.boot.service.MemberService;
 
 @Controller
@@ -12,6 +18,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model){
+        List<Member> members = memberService.findAll();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberService.join(member);
+        return "redirect:/";
     }
 
 }
